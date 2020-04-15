cb0f38f6c0ec79148fcaa9430968b80802fcef2d
kaseypcantu <kaseypaulcantu@gmail.com>
2020-04-14 20:36:46 -0500

    Initial Commit of my CodeCamp API in ASP.NET Core

---
+++ CodeCamp/Data/IDbContextFactory.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace CodeCamp.Data
+{
+    public interface IDbContextFactory
+    {
+    }
+}
---
+++ CodeCamp/Startup.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeCamp.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CodeCamp {
+    public class Startup {
+        private string _saAccessKey = null;
+        private string _connection = null;
+
+        public Startup (IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices (IServiceCollection services) {
+            _saAccessKey = Configuration["CodeCampDataBase:SA_PASSWORD"];
+
+            var builder = new SqlConnectionStringBuilder (
+                Configuration.GetConnectionString ("CodeCamp"));
+            builder.Password = _saAccessKey;
+            _connection = builder.ConnectionString;
+
+            services.AddDbContextPool<CampContext> (options => {
+                options.UseSqlServer (_connection);
+            });
+
+            services.AddControllers ();
+
+            services.AddDbContext<CampContext> ();
+
+            services.AddScoped<ICampRepository, CampRepository> ();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            var builder = new ConfigurationBuilder ()
+                .SetBasePath (env.ContentRootPath)
+                .AddJsonFile ("appsettings.json",
+                    optional : false,
+                    reloadOnChange : true)
+                .AddEnvironmentVariables ();
+
+            if (env.IsDevelopment ()) {
+                builder.AddUserSecrets<Startup> ();
+
+                app.UseDeveloperExceptionPage ();
+            }
+
+            app.UseHttpsRedirection ();
+
+            app.UseRouting ();
+
+            app.UseAuthorization ();
+
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
+            });
+        }
+    }
+}
